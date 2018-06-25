185635b435a5d50de665ab97dacd8a2fdbb434ea
Sithira Munasinghe <sithiraac@gmail.com>
2018-06-25 05:29:52 -0700

    Setup for Mars

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel for Mars
                 </div>
 
                 <div class="links">
